5caeb9c5885cde8b4bdae75e3a64fa92fca9edb3
Jordan Bonnett <bonnettjordan@gmail.com>
2021-04-27 21:49:12 +0000

    FIXING 1-file, if number < 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("last digit of {:d} is".format(number), end=" ")
+print("Last digit of {:d} is".format(number), end=" ")
 
 result = number % 10
-if (number > 0):
+if (number < 0):
     result = number % -10
 
 if result < 6 and result != 0:
