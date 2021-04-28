3dcd532aa6abebc7bfcac8cbdc801f6129eabbc8
Jordan Bonnett <bonnettjordan@gmail.com>
2021-04-28 00:09:29 +0000

    FIXING 1-file and is 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ if (number < 0):
 if result < 6 and result != 0:
     print("{:d} and is less than 6 and not 0".format(result))
 elif result == 0:
-    print("{:d} and is zero".format(result))
+    print("{:d} and is 0".format(result))
 else:
     print("{:d} and is greater than 5".format(result))
