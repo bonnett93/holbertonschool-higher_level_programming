520452566f399ff4921fb9c9c453ff573564d141
Jordan Bonnett <bonnettjordan@gmail.com>
2021-04-28 00:50:28 +0000

    TRIYING 1-file, if ()

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ result = number % 10
 if (number < 0):
     result = number % -10
 print("Last digit of {} is ".format(number), end="")
-if (result) > 5:
+if result > 5:
     print("{} and is greater than 5".format(result))
-elif (result) == 0:
+elif result == 0:
     print("{} and is 0".format(result))
 else:
     print("{} and is less than 6 and not 0".format(result))
