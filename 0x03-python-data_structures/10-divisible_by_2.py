4a8e8f4086ff5bbb0c8c7f13153d0bcb91e5d625
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-06 04:58:32 +0000

    Upload 10 file

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) > 0:
+        new_list = []
+        for i in my_list:
+            if i % 2 == 0:
+                my_list.append(True)
+            else:
+                my_list.append(False)
+    return (my_list)
+
