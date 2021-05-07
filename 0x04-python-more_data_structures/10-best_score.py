043b28921b90704a59e7bf2d303d95592d952340
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-07 18:58:30 +0000

    upload 10-file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-if a_dictionary:
-    score = max(sorted(a_dictionary.values()))
-    for key, value in a_dictionary.items():
-        if value == score:
-            return key
+    if a_dictionary:
+        score = max(sorted(a_dictionary.values()))
+        for key, value in a_dictionary.items():
+            if value == score:
+                return key
