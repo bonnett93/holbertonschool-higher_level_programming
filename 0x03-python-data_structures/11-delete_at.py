85487ba197a3d1d3e028ecedf1264abfe28babdf
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-07 01:35:21 +0000

    Upload 11 file

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,5 +6,7 @@ def delete_at(my_list=[], idx=0):
     for i in range(len(my_list)):
         if i != idx:
             new_list.append(my_list[i])
-    my_list = new_list.copy()
+    my_list.clear()
+    for i in new_list:
+        my_list.append(i)
     return my_list
