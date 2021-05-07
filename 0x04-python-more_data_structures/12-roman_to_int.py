ef75727e29acbf4abbc071db3adefe273c1a6a78
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-07 21:28:00 +0000

    Upload 12-file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,8 @@ def roman_to_int(roman_string):
         one = ['V', 'X']
         ten = ['L', 'C']
         hun = ['D', 'M']
-        romans = {"I": 1, "V": 5, "X": 10, "L": 50, 
-                    "C": 100, "D": 500, "M": 1000}
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
         for i in range(len(roman_string) - 1):
             if roman_string[i] == 'I':
                 if roman_string[i + 1] in one:
