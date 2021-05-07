5959c59362149b37af6cd635644100abd836823e
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-07 21:25:57 +0000

    Upload 12-file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    integer = 0
+    if type(roman_string) == str:
+        one = ['V', 'X']
+        ten = ['L', 'C']
+        hun = ['D', 'M']
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50, 
+                    "C": 100, "D": 500, "M": 1000}
+        for i in range(len(roman_string) - 1):
+            if roman_string[i] == 'I':
+                if roman_string[i + 1] in one:
+                    integer -= 1
+                else:
+                    integer += 1
+            elif roman_string[i] == 'X':
+                if roman_string[i + 1] in ten:
+                    integer -= 10
+                else:
+                    integer += 10
+            elif roman_string[i] == 'C':
+                if roman_string[i + 1] in hun:
+                    integer -= 100
+                else:
+                    integer += 100
+            else:
+                integer += romans[roman_string[i]]
+        integer += romans[roman_string[-1]]
+        return integer
