cbdda2b2a86e1369a435eee958fb59f5b79b4aad
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-08 02:56:38 +0000

    Upload 100-file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list):
+    average = 0 
+    if my_list:
+        denominator = 0
+        divisor = 0
+        for tuple_pair in my_list:
+            denominator += tuple_pair[0] * tuple_pair[1]
+            divisor += tuple_pair[1]
+        average = denominator / divisor    
+    return average
