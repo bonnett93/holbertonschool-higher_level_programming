3f234cee25a6ce1a6fd92bd37fbbf23fdbfafd4b
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-08 03:33:06 +0000

    Upload 102 file

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     if a_dictionary:
+        keys_del = []
         for key, content in a_dictionary.items():
             if content == value:
-                del a_dictionary[key]
+                keys_del.append(key)
+        for key in keys_del:
+            del a_dictionary[key]
     return a_dictionary
