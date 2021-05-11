7736d00eb55fc2231382a57e5126b742459e9925
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-11 04:38:50 +0000

    FIX, 100 file, spaces and tabs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,23 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 if __name__ == "__main__":
-	argc = len(argv)
-	if argc != 4:
-		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-		exit(1)
-	if argv[2] not in ["+", "-", "*", "/"]:
-		print("Unknown operator. Available operators: +, -, * and /")
-		exit(1)
-	a = int(argv[1])
-	b = int(argv[3])
-	if argv[2] == "+":
-		result = add(a, b)
-	elif argv[2] == "-":
-		result = sub(a, b)
-	elif argv[2] == '*':
-		result = mul(a, b)
-	elif argv[2] == "/":
-		result = div(a, b)
-	print("{} {} {} = {}".format(a, argv[2], b, result))
+    argc = len(argv)
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        result = add(a, b)
+    elif argv[2] == "-":
+        result = sub(a, b)
+    elif argv[2] == '*':
+        result = mul(a, b)
+    elif argv[2] == "/":
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, argv[2], b, result))
 
 
