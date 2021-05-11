7fdf8cd8edc80ec5d12932be5e68a27d3b95732a
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-11 01:30:42 +0000

    Upload 7-file

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary:
-        for key, value in a_dictionary.items():
-            a_dictionary[key] = value
+        a_dictionary[key] = value
     return a_dictionary
