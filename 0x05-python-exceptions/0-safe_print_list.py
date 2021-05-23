448682c10672846f7eaefb4c44e42cd999974b2d
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-23 18:28:27 +0000

    Upload 0-file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             elements += 1
-        print("")
     except:
         pass
     finally:
+        print("")
         return elements
