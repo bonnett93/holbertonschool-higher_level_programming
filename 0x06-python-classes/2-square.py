0b41635861c562e5ae4510799edcc9681a57c772
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-23 03:53:28 +0000

    Fix: 2-file, raise errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,6 +16,8 @@ class Square:
             else:
                 self.__size = size
         except TypeError:
-            print("size must be an integer")
+            print("size must be an integer", end="")
+            raise
         except ValueError:
-            print("size must be >= 0")
+            print("size must be >= 0", end="")
+            raise
