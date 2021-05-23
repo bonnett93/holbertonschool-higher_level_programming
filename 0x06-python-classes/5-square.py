2102437f81ccc28a59c83bcf578863a8a933a25f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-23 04:53:22 +0000

    upload 5-file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square with size and area"""
+
+
+class Square:
+    """Create a class Square with:
+        -Instantiation:
+            def __init__(self, size=0)
+                -Private instance attribute:
+                    size
+        -Public instance method:
+            def area(self)
+        -Public instance method:
+            def area(self)
+            def my_print(self)
+        -Getter
+            def size(self):
+        -Setter
+            def size(self, value):"""
+
+    def __handle_error(self, size):
+        """Check if the value of size is ok"""
+        try:
+            if type(size) != int:
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise
+
+    def __init__(self, size=0):
+        """Init is the instantiated Class constructor"""
+        self.__handle_error(size)
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for j in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print("")
+
+    @property
+    def size(self):
+        """Retrieve value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the value of size"""
+        self.__handle_error(value)
+        self.__size = value
