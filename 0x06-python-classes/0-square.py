96c5052c96227b84f3768577e50ef9cb3dfc24c7
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-23 01:51:19 +0000

    Playing, 0-file, documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
+"""Module of task 0-square.py"""
 class Square:
-    """
-        This Class Write an empty class Square that defines a square.
-    """
+    """This Class Write an empty class Square that defines a square."""
     pass
