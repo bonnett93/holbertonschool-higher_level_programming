c04f2900713cd3712f8f832ed22579a12392f448
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-23 02:54:05 +0000

    Upload: 1-file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a Class with private instance attribute: size"""
+
+
+class Square:
+    """This Class Write a class Square that defines a square with size"""
+    def __init__(self, size):
+        """Init is the Class constructor"""
+        self.__size = size
