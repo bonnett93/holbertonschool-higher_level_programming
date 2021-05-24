413a173ad56d8f22e75d7e6cc5aa755f5e5b2147
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-24 02:31:03 +0000

    Change 1-file, order instance attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         self.__handle_error(width, height)
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
