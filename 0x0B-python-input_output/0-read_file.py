3cc757e1ed0e3335092a60c5e7a19ba6413c62f0
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 02:11:04 +0000

    Doc: 0-file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+'''File: 0. Read file'''
 
 def read_file(filename=""):
+    ''' reads a text file (UTF8) and prints it to stdout:'''
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
