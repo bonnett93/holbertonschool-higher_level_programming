83ec8708b1a23f0e2ef014dbf69cabff54a11c45
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 03:13:47 +0000

    Change, 2-file doc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''file: 2. append write'''
 
+
 def append_write(filename="", text=""):
     '''appends a string at the end of a text file (UTF8)
     and returns the number of characters added:'''
