e6c46a0067b9ff81d772b231c94395f7ede77253
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 03:30:13 +0000

    Change: 6-file doc

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+'''File: 6. load from json'''
 import json
 
 
 def load_from_json_file(filename):
+    '''Creates an Object from a “JSON file'''
     with open(filename, "r+") as f:
         json.load(f)
