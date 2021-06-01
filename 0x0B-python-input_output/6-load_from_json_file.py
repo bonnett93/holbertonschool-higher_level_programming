8431ae81991cffe37cc2b49d687e7b5e7834f018
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 03:33:24 +0000

    Fix: 6-file, UTF-8

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     '''Creates an Object from a “JSON file'''
-    with open(filename, "r+") as f:
+    with open(filename, "r+", encoding="UTF-8") as f:
         return json.load(f)
