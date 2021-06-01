7fd30652d719ca29f2843842a1f7ab76215c7175
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 02:42:05 +0000

    Change, 0-file doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Module File: 0. Read file'''
+"""Module File: 0. Read file"""
 
 def read_file(filename=""):
-    '''Reads a text file (UTF8) and prints it to stdout'''
+    """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
