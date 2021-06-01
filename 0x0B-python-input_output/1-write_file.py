d58a3bb18852223466604247d36f6443e860d60b
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-01 02:45:29 +0000

    Change, 1-file doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+'''File: 1. Write file'''
 
 
 def write_file(filename="", text=""):
+    '''writes a string to a text file (UTF8)
+    and returns the number of characters written'''
     with open(filename, "w", encoding="UTF-8") as f:
         characters = f.write(text)
     return characters
