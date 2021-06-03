84423d8eadd6651d7963eef5d954be103b82a8e9
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-03 04:15:11 +0000

    Try: 8-file {:s}

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,9 +11,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         '''Validates value Type and Value'''
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
