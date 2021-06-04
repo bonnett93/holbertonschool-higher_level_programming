1fe6c3b4eb87ac8b4df7868efe579828a26b018f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-04 22:48:14 +0000

    Fix 11-file, change __str__ and validation order

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,9 +8,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     '''Class Square, inheritance from Rectangle'''
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         '''Description of Rectangle'''
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
