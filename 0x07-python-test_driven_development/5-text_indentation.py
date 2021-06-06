57bf309ebe4e02e2bb0a25805986229f48d37ad2
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-06 05:47:29 +0000

    Upload 5-file and 5-test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module 5: text indentation'''
+
+
+def text_indentation(text):
+    '''prints a text with 2 new lines after each of
+        these characters: ., ? and :'''
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    if text[0] != " ":
+        print(text[0], end="")
+    for i in range(1, len(text)):
+        if (text[i-1] in ".?:"):
+            print("")
+            if (text[i] == " "):
+                continue
+        else:
+            print(text[i], end="")
