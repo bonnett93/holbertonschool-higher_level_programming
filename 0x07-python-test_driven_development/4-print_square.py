7c110e3cfc36596a16bbbde5cb3617b28f7a6aea
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-06 03:17:56 +0000

    Change 4-file, case 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,8 +8,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print("")
-        return
     for i in range(size):
         print("#"*size)
