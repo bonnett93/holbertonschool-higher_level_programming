3d4ee2b3f95f5a7e27078077a06f0f299a91c3e8
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-06 07:07:35 +0000

    Fix: 5-file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,10 +10,11 @@ def text_indentation(text):
 
     if text[0] != " ":
         print(text[0], end="")
+        control = 1
     for i in range(1, len(text)):
         if (text[i-1] in ".?:"):
             print("\n")
             control = 0
-        elif text[i] != " " or control == 1:
+        if text[i] != " " or control == 1:
             print(text[i], end="")
             control = 1
