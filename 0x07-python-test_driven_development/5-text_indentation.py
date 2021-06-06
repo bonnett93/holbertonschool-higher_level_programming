91feeed49c1ec88994feb91995f96d9097a1ee01
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-06 06:19:34 +0000

    Fix: 5-file and 5-test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,11 +7,12 @@ def text_indentation(text):
         these characters: ., ? and :'''
     if type(text) != str:
         raise TypeError("text must be a string")
+
     if text[0] != " ":
         print(text[0], end="")
     for i in range(1, len(text)):
         if (text[i-1] in ".?:"):
-            print("")
+            print("\n")
             if (text[i] == " "):
                 continue
         print(text[i], end="")
