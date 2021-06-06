67341901a2fdc2a5df1617988337829e1a7e6c1e
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-06 00:01:14 +0000

    Upload 2-file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Module 2: matrix divided'''
+
+
+def matrix_divided(matrix, div):
+    new_matrix = []
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if type == 0:
+        raise ZeroDivisionError("division by zero")
+    size = len(matrix[0])
+    matrix_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    for row in matrix:
+        new_row = []
+        if size != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError(matrix_msg)
+            new_row.append(round(element / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
