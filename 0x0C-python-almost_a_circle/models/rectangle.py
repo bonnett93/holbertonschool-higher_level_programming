228564198461634416da269039a1e4efd0855b38
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-08 21:33:38 +0000

    Doc: recntagle.py functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,6 +15,7 @@ class Rectangle(Base):
         super().__init__(id)
 
     def __setter_validator(self, name, value):
+        '''Validate if attribute is set correctly'''
         if type(value) != int:
             raise TypeError(name + " must be an integer")
         if name in ['width', 'height']:
@@ -26,36 +27,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''Return width value'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''Set width value'''
         self.__setter_validator("width", value)
         self.__width = value
 
     @property
     def height(self):
+        '''Return height value'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''Set height value'''
         self.__setter_validator("height", value)
         self.__height = value
 
     @property
     def x(self):
+        '''Return x value'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''Set x value'''
         self.__setter_validator("x", value)
         self.__x = value
 
     @property
     def y(self):
+        '''Return y value'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''Set y value'''
         self.__setter_validator("y", value)
         self.__y = value
+
+    def area(self):
+        '''Calculate and return the area of rectangle'''
+        return self.__width * self.__height
