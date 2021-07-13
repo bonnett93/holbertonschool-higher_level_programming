60c459a673f4f2e6a44fd0329f96a1cf69099818
Jordan Bonnett <bonnettjordan@gmail.com>
2021-07-13 22:28:33 +0000

    Upload Files

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges on your server (in localhost).
+-- User user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates the MySQL server user with all privileges.
+-- The user_0d_1 password should be set to user_0d_1_pwd.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
