ca1b0b6935a167d36656b52a8897b0172364113a
Jordan Bonnett <bonnettjordan@gmail.com>
2021-07-13 23:50:53 +0000

    Upload task, 6,7,8

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table states.
 -- id INT unique, auto generated, can’t be null and is a primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa and the table cities.
+-- State_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
