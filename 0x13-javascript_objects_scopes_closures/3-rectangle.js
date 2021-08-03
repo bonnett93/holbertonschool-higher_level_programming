453beb3012747bbe37c8df2a72140683a41d79e5
bonnett93 <bonnettjordan@gmail.com>
2021-08-03 18:47:58 -0500

    Fix semistandard rules

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-	  if (w >= 1 && h >= 1) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-
-	print() {
-		let i;
-		let row = '';
-		for (i = 0; i < this.width; i++) {
-			row = row + 'X'
-		}
-		for (i = 0; i < this.height; i++) {
-			console.log(row);
-		}
-	}
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
   }
+
+  print () {
+    let i;
+    let row = '';
+    for (i = 0; i < this.width; i++) {
+      row = row + 'X';
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
 
 module.exports = Rectangle;
