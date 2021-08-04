2ccdbb3708d4adb20c22ab3845b8baa7f99b2ac2
bonnett93 <bonnettjordan@gmail.com>
2021-08-04 14:47:42 -0500

    Fix semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const myArgv = process.argv.slice(2);
-if(myArgv.length === 0) {
+if (myArgv.length === 0) {
   console.log('No argument');
 } else {
   console.log(myArgv[0]);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (element of array) {
+for (const element of array) {
   console.log(element);
 }
