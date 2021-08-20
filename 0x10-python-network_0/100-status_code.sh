767869adaa0658c898f6043ccf9ee7877af1919f
bonnett93 <bonnettjordan@gmail.com>
2021-08-19 23:10:13 -0500

    Upload 100 status, fix shebang

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response.
 curl -o /dev/null -s --head --write-out '%{http_code}' $1
