5b41060c686a5ae5f9432ce8dcd9843511575f37
bonnett93 <bonnettjordan@gmail.com>
2021-08-20 22:59:40 -0500

    Upload task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module: 5-hbtn_header.py:
+    script that takes in a URL, sends a request to the URL and displays
+    the value of the variable X-Request-Id in the response header"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
