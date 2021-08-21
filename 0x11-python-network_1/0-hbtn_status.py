f53a854f22691449555832d5d80cea375c27d809
bonnett93 <bonnettjordan@gmail.com>
2021-08-20 20:36:44 -0500

    Fix space in -type

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("    - type: ", type(html))
+        print("    - type:", type(html))
         print("    - content:", html)
         print("    - utf8 content:", html.decode("utf-8"))
