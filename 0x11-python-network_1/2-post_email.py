03465d51a2fdb305f742a017be8c1d693f24d184
bonnett93 <bonnettjordan@gmail.com>
2021-08-20 22:36:24 -0500

    Change doc

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Module: 1-hbtn_header.py:
-    takes in a URL, sends a request to the URL and displays the value of
-    the X-Request-Id variable found in the header of the response"""
+"""Module: 2-post_email.py:
+     takes in a URL and an email, sends a POST request to the passed URL
+     with the email as a parameter, and displays the body of the response
+     (decoded in utf-8)"""
 
 if __name__ == '__main__':
-    import urllib.request
-    import urllib.parse
-    import sys
+import urllib.request
+import urllib.parse
+import sys
 
-    data = urllib.parse.urlencode({'email': sys.argv[2]})
-    data = data.encode('ascii')
-    url = sys.argv[1]
-    with urllib.request.urlopen(url, data) as response:
-        print(response.read().decode('utf-8'))
+data = urllib.parse.urlencode({'email': sys.argv[2]})
+data = data.encode('ascii')
+url = sys.argv[1]
+with urllib.request.urlopen(url, data) as response:
+    print(response.read().decode('utf-8'))
