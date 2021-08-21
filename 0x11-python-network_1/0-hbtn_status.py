a7a3102cb72f7348b3243d96fbd53cf358b69c73
bonnett93 <bonnettjordan@gmail.com>
2021-08-20 20:33:55 -0500

    Upload 0-file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module: 0-hbtn_stattus.py:
+    script that fetches https://intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("    - type: ", type(html))
+        print("    - content:", html)
+        print("    - utf8 content:", html.decode("utf-8"))
