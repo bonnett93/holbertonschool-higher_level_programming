26d3cfb6b29af536c45acd1f0ef4c966bec11179
bonnett93 <bonnettjordan@gmail.com>
2021-08-20 23:11:50 -0500

    Upload task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module: 6-post_email.py:
+    script that takes in a URL and an email address, sends a POST request
+    to the passed URL with the email as a parameter, and finally displays
+    the body of the response."""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    payload = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], data=payload)
+    print(req.text)
