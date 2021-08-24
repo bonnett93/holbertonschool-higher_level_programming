dcd73d5b76cbe36f8f54ff337b140cb846854382
bonnett93 <bonnettjordan@gmail.com>
2021-08-24 11:29:44 -0500

    Upload task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,16 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         print('No result')
-    q = sys.argv[1]
-    payload = {'email': sys.argv[1]}
-    url = 'http://0.0.0.0:5000/search_user'
-    req = requests.post(url, data=payload)
-    try:
-        res = req.json()
-        if res:
-            print("[{}] {}".format(res.get('id'), res.get('name')))
-        else:
-            print("No result")
-    except:
-        print("Not a valid JSON")
+    else:
+        q = sys.argv[1]
+        payload = {'email': sys.argv[1]}
+        url = 'http://0.0.0.0:5000/search_user'
+        req = requests.post(url, data=payload)
+        try:
+            res = req.json()
+            if res:
+                print("[{}] {}".format(res.get('id'), res.get('name')))
+            else:
+                print("No result")
+        except:
+            print("Not a valid JSON")
