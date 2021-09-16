4fe7b8273997d0d4ed4b6ae45048285223f815c9
bonnett93 <bonnettjordan@gmail.com>
2021-09-15 22:12:33 -0500

    Fix semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,9 +8,12 @@ request(url, (err, response) => {
   if (err) {
     console.log(err);
   }
-  const characters = JSON.parse(response.body).characters
-  for (character of characters) {
+  const characters = JSON.parse(response.body).characters;
+  for (const character of characters) {
     request(character, (err, response) => {
+      if (err) {
+        console.log(err);
+      }
       console.log(JSON.parse(response.body).name);
     });
   }
